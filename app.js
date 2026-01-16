bdcd059ba52361220f7af441c8ef7abc36ac1018
trocipon <thibaud.rocipon@protonmail.com>
2026-01-17 00:54:04 +0100

    config multer middleware | add multer to routes and controller to manage images

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bookRoutes = require("./routes/book");
 const userRoutes = require("./routes/user");
+const path = require("path");
 
 const username = "trocipon";
 const password = "test0*?";
@@ -35,5 +36,6 @@ app.use((req, res, next) => {
 
 app.use("/api/books", bookRoutes);
 app.use("/api/auth", userRoutes);
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 module.exports = app;
--- routes/book.js
+++ routes/book.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
 const router = express.Router();
 
 const bookCtrl = require("../controllers/book");
 
-router.post("/", auth, bookCtrl.createBook);
-router.put("/:id", auth, bookCtrl.modifyBook);
+router.post("/", auth, multer, bookCtrl.createBook);
+router.put("/:id", auth, multer, bookCtrl.modifyBook);
 router.delete("/:id", auth, bookCtrl.deleteBook);
 router.get("/:id", bookCtrl.getOneBook);
 router.get("/bestrating", bookCtrl.getBestRatingBooks);
