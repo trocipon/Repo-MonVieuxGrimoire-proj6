83291bb2931f3452ce10e52ce72de8c2d90b4aaf
trocipon <thibaud.rocipon@protonmail.com>
2026-01-16 22:28:20 +0100

    create Book model & book controller

---
+++ models/Book.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const bookSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  year: { type: Number, required: true },
+  genre: { type: String, required: true },
+  userId: { type: String, required: true },
+  ratings: { type: [{ userId: String, grade: Number }], required: true },
+  averageRating: { type: Number, required: true },
+});
+
+module.exports = mongoose.model("Book", bookSchema);
