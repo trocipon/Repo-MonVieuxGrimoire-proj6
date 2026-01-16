304da91a2a6534a3d15f551ba0c5a98d50da9adb
trocipon <thibaud.rocipon@protonmail.com>
2026-01-16 23:22:01 +0100

    add getBestRatingBooks middleware in book controller

--- controllers/book.js
+++ controllers/book.js
@@ -29,6 +29,14 @@ exports.getOneBook = (req, res, next) => {
     .catch((error) => res.status(404).json({ error }));
 };
 
+exports.getBestRatingBooks = (req, res, next) => {
+  Book.find()
+    .sort({ averageRating: -1 })
+    .limit(3)
+    .then((books) => res.status(200).json(books))
+    .catch((error) => res.status(400).json({ error }));
+};
+
 exports.getAllBooks = (req, res, next) => {
   Book.find()
     .then((books) => res.status(200).json(books))
