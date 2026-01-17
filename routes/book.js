1cf3d76ab63a7965cbef47dc2e8b25ed9c6850c8
trocipon <thibaud.rocipon@protonmail.com>
2026-01-17 15:44:11 +0100

    books : multer refactor (upload, processImage) + add rating endpoint + reorganize routes

--- routes/book.js
+++ routes/book.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const auth = require("../middleware/auth");
-const multer = require("../middleware/multer-config");
+const { upload, processImage } = require("../middleware/multer-config");
 const router = express.Router();
 
 const bookCtrl = require("../controllers/book");
 
-router.post("/", auth, multer, bookCtrl.createBook);
-router.put("/:id", auth, multer, bookCtrl.modifyBook);
+router.post("/", auth, upload, processImage, bookCtrl.createBook);
+router.put("/:id", auth, upload, processImage, bookCtrl.modifyBook);
 router.delete("/:id", auth, bookCtrl.deleteBook);
-router.get("/:id", bookCtrl.getOneBook);
 router.get("/bestrating", bookCtrl.getBestRatingBooks);
+router.get("/:id", bookCtrl.getOneBook);
 router.get("/", bookCtrl.getAllBooks);
+router.post("/:id/rating", auth, bookCtrl.addRating);
 
 module.exports = router;
