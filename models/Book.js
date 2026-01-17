31618972f8f103069b04ed08e73e70a822a52f86
trocipon <thibaud.rocipon@protonmail.com>
2026-01-17 14:36:50 +0100

    reorganize paths in Book model

--- models/Book.js
+++ models/Book.js
@@ -1,14 +1,25 @@
 const mongoose = require("mongoose");
 
 const bookSchema = mongoose.Schema({
+  userId: { type: String, required: true },
   title: { type: String, required: true },
   author: { type: String, required: true },
   imageUrl: { type: String, required: true },
   year: { type: Number, required: true },
   genre: { type: String, required: true },
-  userId: { type: String, required: true },
-  ratings: { type: [{ userId: String, grade: Number }], required: true },
-  averageRating: { type: Number, required: true },
+  ratings: {
+    type: [
+      {
+        userId: String,
+        grade: Number,
+      },
+    ],
+    required: true,
+  },
+  averageRating: {
+    type: Number,
+    required: true,
+  },
 });
 
 module.exports = mongoose.model("Book", bookSchema);
