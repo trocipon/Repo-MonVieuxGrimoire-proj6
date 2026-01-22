4bd8e7a5474178e7ccd9abbc2aa16a299798ea91
trocipon <thibaud.rocipon@protonmail.com>
2026-01-22 16:23:14 +0100

    user controller : fix JWT_SECRET reference

--- controllers/user.js
+++ controllers/user.js
@@ -36,7 +36,7 @@ exports.login = (req, res, next) => {
           }
           res.status(200).json({
             userId: user._id,
-            token: jwt.sign({ userId: user._id }, process.env.JWT, { expiresIn: "24h" }),
+            token: jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "24h" }),
           });
         })
         .catch((error) => res.status(500).json({ error }));
