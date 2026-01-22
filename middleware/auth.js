a2ed11252749e019a4add328a6a61fb2e529f53f
trocipon <thibaud.rocipon@protonmail.com>
2026-01-22 16:32:07 +0100

    auth controller : fix JWT_SECRET reference

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    const decodedToken = jwt.verify(token, process.env.JWT);
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
     const userId = decodedToken.userId;
     req.auth = { userId: userId };
     next();
